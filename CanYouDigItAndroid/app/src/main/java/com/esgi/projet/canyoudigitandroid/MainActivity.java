7a896de98f1012b12f296fb1de599f99ca883108
jkn1092 <jkn1092@gmail.com>
2015-01-22 15:57:49 +0100

    Commit

--- CanYouDigItAndroid/app/src/main/java/com/esgi/projet/canyoudigitandroid/MainActivity.java
+++ CanYouDigItAndroid/app/src/main/java/com/esgi/projet/canyoudigitandroid/MainActivity.java
@@ -1,19 +1,32 @@
 package com.esgi.projet.canyoudigitandroid;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    Button buttonAjouter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAjouter = (Button) findViewById(R.id.ajouterNote);
+
     }
 
+    public void ajouterUneNote(View v){
+        Intent intent = new Intent(this,NoteActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
