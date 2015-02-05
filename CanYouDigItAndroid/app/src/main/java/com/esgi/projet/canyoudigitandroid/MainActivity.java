e9eeead30f52fac3a35c709872a4f9141ef60cd1
jkn1092 <jkn1092@gmail.com>
2015-02-05 22:22:05 +0100

    push

--- CanYouDigItAndroid/app/src/main/java/com/esgi/projet/canyoudigitandroid/MainActivity.java
+++ CanYouDigItAndroid/app/src/main/java/com/esgi/projet/canyoudigitandroid/MainActivity.java
@@ -2,6 +2,7 @@ package com.esgi.projet.canyoudigitandroid;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -19,36 +21,29 @@ import org.w3c.dom.Text;
 public class MainActivity extends Activity {
 
     static final String STATE_BLOC_NOTES = "STATE_BLOC_NOTES";
-    private BlocNotes monBlocNotes;
-    Button buttonAjouter;
-    TableLayout tableLayoutNotes;
+    static final String STATE_RECHERCHE = "RECHERCHE";
+    BlocNotes monBlocNotes;
+    EditText editTexteRechercheNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonAjouter = (Button) findViewById(R.id.ajouterNote);
+        TableLayout tableLayoutNotes = (TableLayout) findViewById(R.id.tableNote);
+        editTexteRechercheNotes = (EditText) findViewById(R.id.rechercheNote);
 
         if(savedInstanceState != null){
-            monBlocNotes = savedInstanceState.getParcelable(STATE_BLOC_NOTES);
-            Log.v("TAG","Ancien");
-        }
-
-        if(monBlocNotes == null){
-            Log.v("Blocnote","null");
+            monBlocNotes = (BlocNotes)savedInstanceState.getParcelable(STATE_BLOC_NOTES);
+            editTexteRechercheNotes.setText(savedInstanceState.getString(STATE_RECHERCHE));
+        }else{
             monBlocNotes = new BlocNotes();
         }
 
-        setContentView(R.layout.activity_main);
-
-        buttonAjouter = (Button) findViewById(R.id.ajouterNote);
-        tableLayoutNotes = (TableLayout) findViewById(R.id.tableNote);
-
-
         if(getIntent().hasExtra("nouvelleNote")) {
-            Log.v("TAG","JAI UNE NEW NOTE");
-            Note nouvelleNote = getIntent().getExtras().getParcelable("nouvelleNote");
+            Note nouvelleNote = getIntent().getParcelableExtra("nouvelleNote");
             monBlocNotes.ajouterNote(nouvelleNote);
-        }else{
-            Log.v("TAG","JAI PAS DE new NOTE");
         }
 
         for(Note n: monBlocNotes.getMesNotes()){
@@ -65,40 +60,9 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    protected void onResume() {
-        Log.v("onResume","Resume");
-        super.onResume();
-    }
-
-    @Override
-    protected void onRestart() {
-        Log.v("onRestart","Restart");
-        super.onRestart();
-    }
-
-    @Override
-    protected void onStop(){
-        super.onStop();
-        Log.v("onStop","Stop");
-    }
-
-    @Override
-    protected void onStart(){
-        super.onStart();
-        Log.v("onStart","Start");
-    }
-
-    @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
-        Log.v("onSaveInstanceState","Mais j'ai save ce foutu bloc notes");
         savedInstanceState.putParcelable(STATE_BLOC_NOTES, monBlocNotes);
-
-        if(savedInstanceState.getParcelable(STATE_BLOC_NOTES) == null){
-            Log.v("onSaveInstanceState","NULL");
-        }else{
-            Log.v("onSaveInstanceState","PAS NULL");
-
-        }
+        savedInstanceState.putString(STATE_RECHERCHE, editTexteRechercheNotes.getText().toString());
 
         super.onSaveInstanceState(savedInstanceState);
     }
@@ -106,28 +70,18 @@ public class MainActivity extends Activity {
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        monBlocNotes = savedInstanceState.getParcelable(STATE_BLOC_NOTES);
-
-        for(Note n: monBlocNotes.getMesNotes()){
-
-            TableRow row = new TableRow(this);
-            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
-            row.setLayoutParams(lp);
-            TextView textTitreNote = new TextView(this);
-
-            textTitreNote.setText(n.getTitre() + " " + n.getDateModif());
-            row.addView(textTitreNote);
-            tableLayoutNotes.addView(row,lp);
-
-        }
-
-        Log.v("onRestoreInstanceState","Je restaure !!!!!!!");
+        monBlocNotes = (BlocNotes)savedInstanceState.getParcelable(STATE_BLOC_NOTES);
+        editTexteRechercheNotes.setText(savedInstanceState.getString(STATE_RECHERCHE));
     }
-
-
 
     public void ajouterUneNote(View v){
         Intent intent = new Intent(this,NoteActivity.class);
+        startActivity(intent);
+    }
+
+    public void parametrerGroupes(View v){
+        Intent intent = new Intent(this,ParametrageActivity.class);
+        intent.putExtra("BlocNotes",(Parcelable)monBlocNotes);
         startActivity(intent);
     }
 
--- CanYouDigItAndroid/app/src/main/java/com/esgi/projet/canyoudigitandroid/ParametrageActivity.java
+++ CanYouDigItAndroid/app/src/main/java/com/esgi/projet/canyoudigitandroid/ParametrageActivity.java
@@ -1,17 +1,31 @@
 package com.esgi.projet.canyoudigitandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class ParametrageActivity extends Activity {
 
+    BlocNotes monBlocNotes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parametrage);
+
+        Intent i = getIntent();
+        monBlocNotes = (BlocNotes) i.getParcelableExtra("BlocNotes");
+
+        if(monBlocNotes == null){
+            Log.v("PARAMETRAGE","NULL y'a un problème ça doit pas etre null et ça marche normalement j'ai testé !");
+        }else{
+            Log.v("PARAMETRAGE","PAS NULL nb notes = " + monBlocNotes.getMesNotes().size());
+        }
+
     }
 
 
