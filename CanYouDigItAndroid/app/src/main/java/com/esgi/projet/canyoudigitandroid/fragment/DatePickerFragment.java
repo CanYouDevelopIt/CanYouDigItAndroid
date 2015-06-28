32572af0cab9718fb2125933f427cb7d292c9793
jkn1092 <jkn1092@gmail.com>
2015-06-29 00:00:21 +0200

    Affichage rappel

--- CanYouDigItAndroid/app/src/main/java/com/esgi/projet/canyoudigitandroid/fragment/DatePickerFragment.java
+++ CanYouDigItAndroid/app/src/main/java/com/esgi/projet/canyoudigitandroid/fragment/DatePickerFragment.java
@@ -5,48 +5,44 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.TextView;
-
-import com.esgi.projet.canyoudigitandroid.DialogCallbackInterface;
 import com.esgi.projet.canyoudigitandroid.R;
 
 import java.util.Calendar;
 
 public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
 
-    public Button rappel;
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-
-        View view = inflater.inflate(R.layout.fragment_note, container);
-        rappel = (Button) view.findViewById(R.id.dthRappel);
-        System.out.println("TESSSSS");
-        return view;
-    }
-
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the current date as the default date in the picker
         final Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
-        String time = "";
 
-        // Create a new instance of DatePickerDialog and return it
         return new DatePickerDialog(getActivity(), this, year, month, day);
     }
 
     public void onDateSet(DatePicker view, int year, int month, int day) {
-        rappel.setText("YESSSSSSSSS");
+
+        String dayChaine;
+        String monthChaine;
+
+        if(day > 9)
+            dayChaine = Integer.toString(day);
+        else
+            dayChaine = "0" + Integer.toString(day);
+
+        if(month > 9)
+            monthChaine = Integer.toString(month);
+        else
+            monthChaine = "0" + Integer.toString(month);
+
+        String dth = dayChaine + "/" + monthChaine + "/" + year;
+
+        DialogFragment timePickerFragment = new TimePickerFragment();
+        timePickerFragment.show(getFragmentManager(), "timePicker");
+        ((Button) getActivity().findViewById(R.id.dthRappel)).setText(dth);
     }
 
 }
--- CanYouDigItAndroid/app/src/main/java/com/esgi/projet/canyoudigitandroid/fragment/TimePickerFragment.java
+++ CanYouDigItAndroid/app/src/main/java/com/esgi/projet/canyoudigitandroid/fragment/TimePickerFragment.java
@@ -6,31 +6,44 @@ import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.TimePicker;
+
+import com.esgi.projet.canyoudigitandroid.R;
 
 import java.util.Calendar;
 
 /**
  * Created by Jkn1092 on 24/06/2015.
  */
-public class TimePickerFragment extends DialogFragment
-        implements TimePickerDialog.OnTimeSetListener {
+public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the current time as the default values for the picker
         final Calendar c = Calendar.getInstance();
         int hour = c.get(Calendar.HOUR_OF_DAY);
         int minute = c.get(Calendar.MINUTE);
 
-        // Create a new instance of TimePickerDialog and return it
-        return new TimePickerDialog(getActivity(), this, hour, minute,
-                DateFormat.is24HourFormat(getActivity()));
+        return new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
     }
 
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        String time = hourOfDay + "" + minute;
-        Log.v("TIME",time);
+        String dth = (String) ((Button) getActivity().findViewById(R.id.dthRappel)).getText();
+        String hourChaine;
+        String minuteChaine;
+
+        if(hourOfDay > 9)
+            hourChaine = Integer.toString(hourOfDay);
+        else
+            hourChaine = "0" + Integer.toString(hourOfDay);
+
+        if(minute > 9)
+            minuteChaine = Integer.toString(minute);
+        else
+            minuteChaine = "0" + Integer.toString(minute);
+
+        dth += " " + hourChaine + "h" + minuteChaine;
+        ((Button) getActivity().findViewById(R.id.dthRappel)).setText(dth);
     }
 
 
